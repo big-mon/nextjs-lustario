9dd2b8214f896b74860773dc40e18250ff065320
Mon <mrmon93+github@gmail.com>
2021-11-05 11:07:36 +0900

    chore: height edit

--- src/components/molecules/ArticleCategory.tsx
+++ src/components/molecules/ArticleCategory.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ArticleCategory = ({ children }: Props) => {
   return (
-    <p className="text-sm opacity-80 mb-2" itemProp="keywords">
+    <p className="text-sm opacity-80 mb-4" itemProp="keywords">
       {children}
     </p>
   );
