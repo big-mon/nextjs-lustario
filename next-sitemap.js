64da8b5afe58edafcf1f043ed95b73c9d86c0680
Mon <mrmon93+github@gmail.com>
2021-11-07 23:17:57 +0900

    feat: support sitemap, progressbar

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://estrilda.damonge.com",
+  generateRobotsTxt: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import GlobalHeader from "components/organisms/GlobalHeader";
 import GlobalFooter from "components/organisms/GlobalFooter";
+import NextProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
+
+      <NextProgress color="rgb(185, 28, 28)" stopDelayMs={100} height={2} />
 
       <GlobalHeader />
       <div className="container mx-auto px-6">
