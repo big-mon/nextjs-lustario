1481ae28d6923c8289ecd087d203b555bed663d0
Mon <mrmon93+github@gmail.com>
2022-02-19 05:15:54 +0900

    chore: edit url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,7 +10,7 @@ export const SITE_ROOT: string = "https://estrilda.damonge.com/";
 
 /** web site OGP default image */
 export const SITE_IMAGE: string =
-  "https://res.cloudinary.com/damonge/image/upload/t_postimage,f_auto/Hero/YS4_5968_69_70-2-3_x9dlrp.jpg";
+  "https://res.cloudinary.com/damonge/image/upload/f_auto,q_auto,fl_progressive,c_limit,h_630,w_1200/Hero/YS4_5968_69_70-2-3_x9dlrp";
 
 /** owner name */
 export const OWNER: string = "MON";
