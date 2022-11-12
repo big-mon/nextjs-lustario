7d179136fedf169d41ed66ba56bc0ca059fc1bf0
MON <mrmon93+git@gmail.com>
2022-11-12 18:21:43 +0900

    build: upgrade

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
   },
 };
+module.exports = nextConfig;
