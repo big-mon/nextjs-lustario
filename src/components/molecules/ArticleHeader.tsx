dbc6b23df8743df0087634677f4934e14d8409cb
MON <mrmon93+git@gmail.com>
2022-11-16 07:01:19 +0900

    refactor: simply heroImage

--- src/components/molecules/ArticleHeader.tsx
+++ src/components/molecules/ArticleHeader.tsx
@@ -1,5 +1,6 @@
 import type { PostMeta } from "models/Post";
-import HeroImage from "components/atoms/HeroImage";
+import Image from "next/image";
+import { convertInnerUrl } from "utils/url";
 import ArticleCategory from "components/molecules/ArticleCategory";
 import ArticleTitle from "components/molecules/ArticleTitle";
 import ArticleDate from "components/molecules/ArticleDate";
@@ -12,7 +13,14 @@ const ArticleHeader = ({ data }: Props) => {
   return (
     <>
       <div className="mx-screen relative h-104 mb-16">
-        <HeroImage src={data.coverImage} alt={data.title} />
+        {/* ヒーローイメージ */}
+        <Image
+          src={convertInnerUrl(data.coverImage)}
+          alt={data.title}
+          layout="fill"
+          objectFit="cover"
+          className="pointer-events-none"
+        />
 
         <div className="absolute bottom-6 max-w-4xl w-full mx-auto p-6 left-1/2 transform -translate-x-2/4 rounded backdrop-filter backdrop-blur-sm bg-black bg-opacity-20 text-gray-200">
           <ArticleCategory>{data.category}</ArticleCategory>
