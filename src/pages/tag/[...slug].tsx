1339275811a49e71b2b5d6eba74a1e12d4af0659
MON <mrmon93+git@gmail.com>
2022-11-16 05:55:24 +0900

    refactor: services -> tag

--- src/pages/tag/[...slug].tsx
+++ src/pages/tag/[...slug].tsx
@@ -1,7 +1,6 @@
 import type { GetStaticPaths, GetStaticProps } from "next";
 import type { PostMeta } from "models/Post";
-import { getSortedPostsMeta } from "repositories/local/markdown/post";
-import { PER_PAGE } from "constants/setting";
+import { getPosts, getAllPaths } from "services/accessToTag";
 import Template from "components/template/Index";
 import SEO from "components/organisms/SEO";
 
@@ -29,19 +28,7 @@ const Page = ({ posts, total, current, tag }: Props) => {
 
 /** 動的なルーティング対象の一覧を定義 */
 export const getStaticPaths: GetStaticPaths = async () => {
-  const allMeta = getSortedPostsMeta();
-  const tags = Array.from(new Set(allMeta.flatMap((meta) => meta.tags)));
-  const paths = tags.flatMap((tag) => {
-    // 指定タグのページ数を計算
-    const postCount = allMeta.filter((meta) => meta.tags.includes(tag)).length;
-    const pageCount = Math.ceil(postCount / PER_PAGE);
-
-    let childParams = [];
-    for (let i = 1; i <= pageCount; i++) {
-      childParams.push([tag.toLowerCase(), i.toString()]);
-    }
-    return childParams;
-  });
+  const paths = getAllPaths();
 
   return {
     paths: paths.map((path) => ({
@@ -58,15 +45,12 @@ export const getStaticProps: GetStaticProps = ({ params }) => {
   const page = Number(slug[1]) ?? 1;
 
   // タグとページに該当する記事に絞り込み
-  const allPosts = getSortedPostsMeta().filter((meta) =>
-    meta.tags.map((tag) => tag.toLowerCase()).includes(tag)
-  );
-  const posts = allPosts.slice((page - 1) * PER_PAGE, page * PER_PAGE);
+  const posts = getPosts(tag, page);
 
   return {
     props: {
-      posts: posts,
-      total: allPosts.length,
+      posts: posts.posts,
+      total: posts.total,
       current: page,
       tag: tag,
     },
---
+++ src/services/accessToTag.ts
@@ -0,0 +1,38 @@
+import { PER_PAGE } from "constants/setting";
+import { getSortedPostsMeta } from "repositories/local/markdown/post";
+
+/** タグ一覧ページ向けの記事一覧とページ数を取得 */
+export const getPosts = (tag: string, page: number) => {
+  // タグとページに該当する記事に絞り込み
+  const allPosts = getSortedPostsMeta().filter((meta) =>
+    meta.tags.map((tag) => tag.toLowerCase()).includes(tag)
+  );
+  const currentPagePosts = allPosts.slice(
+    (page - 1) * PER_PAGE,
+    page * PER_PAGE
+  );
+
+  return {
+    posts: currentPagePosts,
+    total: allPosts.length,
+  };
+};
+
+/** タグ一覧ページのパス一覧を取得 */
+export const getAllPaths = () => {
+  const allMeta = getSortedPostsMeta();
+  const tags = Array.from(new Set(allMeta.flatMap((meta) => meta.tags)));
+  const paths = tags.flatMap((tag) => {
+    // 指定タグのページ数を計算
+    const postCount = allMeta.filter((meta) => meta.tags.includes(tag)).length;
+    const pageCount = Math.ceil(postCount / PER_PAGE);
+
+    let childParams = [];
+    for (let i = 1; i <= pageCount; i++) {
+      childParams.push([tag.toLowerCase(), i.toString()]);
+    }
+    return childParams;
+  });
+
+  return paths;
+};
