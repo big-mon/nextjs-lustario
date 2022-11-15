188c340faabeb060e6fd3c5e63386d13a0dbce60
MON <mrmon93+git@gmail.com>
2022-11-16 07:14:38 +0900

    refactor: simply structure

--- src/components/post/detail/layout.tsx
+++ src/components/post/detail/layout.tsx
@@ -1,7 +1,7 @@
 import type { PostData } from "models/Post";
+import CustomLink from "components/common/CustomLink";
 import ArticleHeader from "components/post/detail/ArticleHeader";
 import ArticleBody from "components/post/detail/ArticleBody";
-import ArticleTags from "components/post/detail/ArticleTags";
 
 type Props = {
   data: PostData;
@@ -9,15 +9,29 @@ type Props = {
 
 /** 記事全文 */
 const ArticleDetail = ({ data }: Props) => {
+  const tags = data.meta.tags;
+
   return (
     <>
       <article itemProp="mainEntityOfPage">
         {/* ヘッダー */}
         <ArticleHeader data={data.meta} />
+
         {/* 本文 */}
         <ArticleBody content={data.content} />
+
         {/* タグ一覧 */}
-        <ArticleTags tags={data.meta.tags} />
+        <div className="max-w-2xl mx-auto py-6 border-t border-gray-500">
+          {tags.map((tag, i) => (
+            <CustomLink
+              key={i}
+              href={`/tag/${tag.toLowerCase()}/1`}
+              className="mr-4"
+            >
+              #{tag}
+            </CustomLink>
+          ))}
+        </div>
       </article>
     </>
   );
